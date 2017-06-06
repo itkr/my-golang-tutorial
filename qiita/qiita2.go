26d01898de437e08324c80bed17efa14a490e982
itkr <itkrst@gmail.com>
2017-06-06 19:19:11 +0900

    csvをs3に保存するようにした

--- qiita/qiita2.go
+++ qiita/qiita2.go
@@ -3,22 +3,60 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
+	"path/filepath"
 	"os"
 	"strconv"
 	"strings"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-const organizationId = "gumi"
+const organizationID = "gumi"
+const region = "ap-northeast-1"
+const bucket = "gumi-qiita"
+const tmpFilePath = "/tmp/qiita.csv"
 
+// Member : メンバーのデータを扱う
 type Member struct {
 	Name          string
 	Contributions int
 }
 
-func getMembersInAPage(organizationId string, page int) ([]Member, error) {
+func putToS3(srcFilePath string) error {
+	file, err := os.Open(srcFilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// 環境変数で AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY を指定する
+	// accessKeyを直接指定も可能（publicリポジトリにコミットしないこと）
+	// creds := credentials.NewStaticCredentials(accessKey, sercetAccessKey, "")
+	creds := credentials.NewEnvCredentials()
+	config := aws.Config{
+		Credentials: creds,
+		Region:      aws.String(region),
+	}
+	sess, err := session.NewSession(&config)
+	if err != nil {
+		return err
+	}
+	client := s3.New(sess)
+	_, fileName := filepath.Split(srcFilePath)
+	_, err = client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(fileName),
+		Body:   file,
+	})
+	return err
+}
+
+func getMembersInAPage(organizationID string, page int) ([]Member, error) {
 	var members []Member
-	url := fmt.Sprintf("https://qiita.com/organizations/%s/members?page=%d", organizationId, page)
+	url := fmt.Sprintf("https://qiita.com/organizations/%s/members?page=%d", organizationID, page)
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
 		return members, err
@@ -43,7 +81,7 @@ func getMembers() ([]Member, error) {
 	var allMembers []Member
 	var maxMemberCountPerPage int
 	for page := 1; ; page++ {
-		membersInAPage, err := getMembersInAPage(organizationId, page)
+		membersInAPage, err := getMembersInAPage(organizationID, page)
 		if err != nil {
 			return allMembers, err
 		}
@@ -61,8 +99,8 @@ func getMembers() ([]Member, error) {
 	return allMembers, nil
 }
 
-func writeToCSV(members []Member) error {
-	file, err := os.OpenFile("./qiita.csv", os.O_WRONLY|os.O_CREATE, 0600)
+func writeToCSV(members []Member, dstFilePath string) error {
+	file, err := os.OpenFile(dstFilePath, os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		return err
 	}
@@ -83,7 +121,11 @@ func main() {
 	for _, member := range members {
 		fmt.Printf("%s: %d\n", member.Name, member.Contributions)
 	}
-	err = writeToCSV(members)
+	err = writeToCSV(members, tmpFilePath)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = putToS3(tmpFilePath)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
