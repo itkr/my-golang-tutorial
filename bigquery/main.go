02bbbbc81c3324a56d5d645878a0efb92bef8613
itkr <itkrst@gmail.com>
2017-06-19 17:05:22 +0900

    BigQueryのデータをcsvに保存する

--- bigquery/main.go
+++ bigquery/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/jwt"
 	"google.golang.org/api/bigquery/v2"
 	"log"
 	"os"
+	"strconv"
 	"time"
+	"reflect"
 )
 
 const tokenURL = "https://accounts.google.com/o/oauth2/token"
 
 const queryBase = `SELECT
-  DATE(event_time) AS segment, os_name, COUNT(DISTINCT app_user_id) AS user_count, '%s' AS game_id
+  DATE(UTC_USEC_TO_MONTH(event_time)) AS segment, os_name, COUNT(DISTINCT app_user_id) AS user_count, '%s' AS game_id
 FROM
   TABLE_DATE_RANGE([%s:%s.%s], TIMESTAMP('%s'), TIMESTAMP('%s'))
 WHERE
@@ -24,11 +27,48 @@ GROUP BY
 ORDER BY
   segment, os_name`
 
+const segmentLayout = "2006-01-02"
+
 type QueryResult struct {
 	Segment   time.Time
 	OSName    string
-	UserCount string
+	UserCount int
 	GameID    string
+}
+
+func (queryResult QueryResult) CSVHeader() []string {
+	rt := reflect.TypeOf(queryResult)
+	fields := make([]string, rt.NumField())
+	for i := 0; i < rt.NumField(); i++ {
+		fields[i] = rt.Field(i).Name
+	}
+	return fields
+}
+
+func (queryResult *QueryResult) ToCSVRaw() []string {
+	return []string{
+		queryResult.Segment.Format(segmentLayout),
+		queryResult.OSName,
+		strconv.Itoa(queryResult.UserCount),
+		queryResult.GameID,
+	}
+}
+
+func writeToCSV(queryResults []*QueryResult, dstFilePath string) error {
+	file, err := os.OpenFile(dstFilePath, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	if len(queryResults) != 0 {
+		writer.Write(queryResults[0].CSVHeader())
+		for _, queryResult := range queryResults {
+			writer.Write(queryResult.ToCSVRaw())
+		}
+	}
+	writer.Flush()
+	return nil
 }
 
 func query(service *bigquery.Service, projectID string, sql string) ([]*QueryResult, error) {
@@ -38,14 +78,18 @@ func query(service *bigquery.Service, projectID string, sql string) ([]*QueryRes
 	}).Do()
 	queryResults := make([]*QueryResult, len(queryResponse.Rows))
 	for i, row := range queryResponse.Rows {
-		t, err := time.Parse("2006-01-02", row.F[0].V.(string))
+		segment, err := time.Parse(segmentLayout, row.F[0].V.(string))
+		if err != nil {
+			continue
+		}
+		userCount, err := strconv.Atoi(row.F[2].V.(string))
 		if err != nil {
 			continue
 		}
 		queryResults[i] = &QueryResult{
-			Segment:   t,
+			Segment:   segment,
 			OSName:    row.F[1].V.(string),
-			UserCount: row.F[2].V.(string),
+			UserCount: userCount,
 			GameID:    row.F[3].V.(string),
 		}
 	}
@@ -111,8 +155,8 @@ func getLastDayOfMonth(t time.Time) time.Time {
 
 func makeQuery(projectID string, t time.Time, appID string) string {
 	dataSet := "dummy"
-	tableNameBase := "dummy"
-	timeStampLayout := "2006-01-02"
+        tableNameBase := "dummy"
+        timeStampLayout := "2006-01-02"
 
 	from := getFirstDayOfMonth(t).Format(timeStampLayout)
 	to := getFirstDayOfMonth(t.AddDate(0, 1, 0)).Format(timeStampLayout)
@@ -120,28 +164,25 @@ func makeQuery(projectID string, t time.Time, appID string) string {
 	return fmt.Sprintf(queryBase, appID, projectID, dataSet, tableNameBase, from, to, from, to)
 }
 
-func execute(config Config, t time.Time) error {
+func getAggregationFromBigQuery(config Config, t time.Time) ([]*QueryResult, error) {
 	service, err := getBigQueryService(config.Email, config.FilePath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	sql := makeQuery(config.ProjectID, t, config.AppID)
 	rows, err := query(service, config.ProjectID, sql)
-	fmt.Println(rows)
-	//jobID, err := insrtQuery(service, config.ProjectID, query)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	//fmt.Println(jobID)
-	//fmt.Println(getQueryResult(service, config.ProjectID, jobID))
-	return nil
+	return rows, nil
 }
 
 func main() {
 	appID := "dummy"
 	config := getConfig()[appID]
 	lastMonth := time.Now().AddDate(0, -1, 0)
-	err := execute(config, lastMonth)
+	rows, err := getAggregationFromBigQuery(config, lastMonth)
+	writeToCSV(rows, "/tmp/mau.csv")
 	if err != nil {
 		log.Fatalf("%v", err)
 		fmt.Printf("%v", err)
